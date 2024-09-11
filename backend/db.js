292673e7c60fa426d1eb6094450df505d2d76ecf
chaman3s <chamanggarwal@gmail.com>
2024-09-11 22:16:38 +0530

    try

--- backend/db.js
+++ backend/db.js
@@ -1,33 +1,24 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI; // Use the environment variable
 
-
-const mongoose = require('mongoose')
-// const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = "mongodb+srv://chamanggarwal:64yfwItZXdudiq2t@cluster0.xftd8.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0/" // Customer change url to your db you created in atlas
-// mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
-    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
-        if (err) console.log("---" + err)
-        else {
-            // var database =
-            console.log("connected to mongo")
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, async (err, result) => {
+        if (err) {
+            console.log("---" + err);
+            callback(err);
+        } else {
+            console.log("Connected to MongoDB");
             const foodCollection = await mongoose.connection.db.collection("food_items");
             foodCollection.find({}).toArray(async function (err, data) {
                 const categoryCollection = await mongoose.connection.db.collection("foodCategory");
                 categoryCollection.find({}).toArray(async function (err, Catdata) {
                     const SliderCollection = await mongoose.connection.db.collection("SliderImage");
-                    SliderCollection.find({}).toArray(async function (err,slideData) {
-                        callback(err, data, Catdata,slideData);
-                        
+                    SliderCollection.find({}).toArray(async function (err, slideData) {
+                        callback(err, data, Catdata, slideData);
                     });
-                    
-
-                })
+                });
             });
-            // listCollections({name: 'food_items'}).toArray(function (err, database) {
-            // });
-            //     module.exports.Collection = database;
-            // });
         }
-    })
+    });
 };
